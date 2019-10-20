52c5b7825a102366bd2ad7b2ce1c891add87db8b
George <he.george10@gmail.com>
2019-10-19 22:18:51 -0400

    UIWork

---
+++ HackathonGenysisAR/Assets/Scripts/ShowFAQ.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFAQ : MonoBehaviour
+{
+    public Button FAQ1;
+    public Button FAQ2;
+    public Button FAQ3;
+    public Button FAQ4;
+    public Button FAQ5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FAQ1.interactable = false;
+        FAQ2.interactable = false;
+        FAQ3.interactable = false;
+        FAQ4.interactable = false;
+        FAQ5.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void test()
+    {
+        Debug.Log("works");
+    }
+
+}
